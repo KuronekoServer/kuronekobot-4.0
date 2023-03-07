4018c97769ec603430c8fb4f45acdc3efa1f0bc9
TanakaTakeshikun <maru1679@icloud.com>
2023-03-07 23:45:18 +0900

    feature: bedrock版の検索機能

---
+++ commands/misc/sub/mcbedrock.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder, Colors } = require("discord.js");
+const { getJson } = require("../../../helpers/HttpUtils");
+module.exports = async (query) => {
+  const response = await getJson(`https://api.mcstatus.io/v2/status/bedrock/${query}`);
+  if (!response?.success) return (`Couldn't find anything for that term...`);
+  const json = response.data;
+  const embed = new EmbedBuilder()
+    .setThumbnail(`https://cdn.mikn.dev/mclogo.png`)
+    .setAuthor({
+      name: `Minecraftサーバー (BedRock)`,
+    })
+    .addFields(
+      {
+        name: `ステータス`,
+        value: json.online ? "オンライン" : "オフライン"
+      },
+      {
+        name: `アドレス`,
+        value: json.host
+      },
+      {
+        name: `ポート`,
+        value: String(json.port)
+      },
+    )
+    .setColor(Colors.Green);
+
+  return ({ embeds: [embed] });
+
+};
