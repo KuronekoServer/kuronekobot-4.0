7341ae6e270d52ed919f7d72952b2a996c86967d
TanakaTakeshikun <maru1679@icloud.com>
2023-03-07 19:33:07 +0900

    format(style):コード整形
    こいつエラー出てるけど大丈夫そ？

--- commands/misc/minecraft.js
+++ commands/misc/minecraft.js
@@ -2,35 +2,33 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const mcjava = require("./sub/mcjava");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('minecraft')
-        .setDescription('Minecraft関係コマンド')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('server-java')
-                .setDescription('Java版Minecraftサーバー情報')
-                .addStringOption(option => option.setName('address').setDescription('サーバーのアドレス')))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('server-bedrock')
-                .setDescription('統合版Minecraftサーバー情報')
-                .addStringOption(option => option.setName('address').setDescription('サーバーのアドレス')))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('user')
-                .setDescription('Minecraftユーザー情報')
-                .addStringOption(option => option.setName('username').setDescription('検索したいユーザーの名前'))),        
-        async execute(interaction) {
-                const sub = interaction.options.getSubcommand();
-                let response;
-            
-                // Javaサーバー
-                if (sub === "server-java") {
-                  const query = interaction.options.getString("address");
-                  response = await mcjava(query, interaction);
-                }
-                
-                await interaction.reply(response);
-              },
-            };
-            
+  data: new SlashCommandBuilder()
+    .setName('minecraft')
+    .setDescription('Minecraft関係コマンド')
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('server-java')
+        .setDescription('Java版Minecraftサーバー情報')
+        .addStringOption(option => option.setName('address').setDescription('サーバーのアドレス')))
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('server-bedrock')
+        .setDescription('統合版Minecraftサーバー情報')
+        .addStringOption(option => option.setName('address').setDescription('サーバーのアドレス')))
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('user')
+        .setDescription('Minecraftユーザー情報')
+        .addStringOption(option => option.setName('username').setDescription('検索したいユーザーの名前'))),
+  async execute(interaction) {
+    const sub = interaction.options.getSubcommand();
+    let response;
+
+    // Javaサーバー
+    if (sub === "server-java") {
+      const query = interaction.options.getString("address");
+      response = await mcjava(query, interaction);
+    };
+    await interaction.reply(response);
+  },
+};
