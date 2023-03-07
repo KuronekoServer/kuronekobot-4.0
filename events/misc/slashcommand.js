851a57e3f354ce18a318dd34f141842a5186c96c
TanakaTakeshikun <maru1679@icloud.com>
2023-03-07 18:41:38 +0900

    fix: エラーハンドリング追加
    よくある権限が足りない場合のメッセージを追加した

--- events/misc/slashcommand.js
+++ events/misc/slashcommand.js
@@ -1,5 +1,8 @@
-const { Events } = require('discord.js');
-
+const { Events, EmbedBuilder, Colors } = require('discord.js');
+const permissions_embed = new EmbedBuilder()
+    .setTitle("⚠️エラー")
+    .setDescription("権限が足りません。\nBOTに権限を与えてください")
+    .setColor(Colors.Red);
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
@@ -11,7 +14,7 @@ module.exports = {
                 await command.autocomplete(interaction);
             } catch (error) {
                 console.error(error);
-            }
+            };
         };
 
         const command = interaction.client.commands.get(interaction.commandName);
@@ -21,7 +24,8 @@ module.exports = {
             await command.execute(interaction);
         } catch (err) {
             console.error(err);
-            await interaction.reply({ content: 'コマンドの実行中にエラーが発生しました。', ephemeral: true });
+            if (err.message === "Missing Permissions") return await interaction.reply({ embeds: [permissions_embed], ephemeral: true }).catch(() => { });
+            await interaction.reply({ content: 'コマンドの実行中にエラーが発生しました。', ephemeral: true }).catch(() => { });
         };
     }
 };
