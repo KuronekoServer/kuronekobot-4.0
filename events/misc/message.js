bdc8f49248722c16e5c9b3428811bcec5e3ef917
TanakaTakeshikun <maru1679@icloud.com>
2023-03-11 11:33:25 +0900

    refactor: 紛らわしいファイル名なので変更

--- events/misc/message.js
+++
@@ -1,9 +0,0 @@
-const { Events } = require('discord.js');
-const response = require("../../messages.json")
-module.exports = {
-    name: Events.MessageCreate,
-    async execute(message) {
-        if (!response[message.content]) return;
-        await message.channel.send({ content: response[message.content] });
-    }
-}
