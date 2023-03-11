12fbc9e18a5897849f0268526a7d72098fca870e
TanakaTakeshikun <maru1679@icloud.com>
2023-03-11 11:32:36 +0900

    fix:sqlがreleaseしない問題を解消

--- commands/misc/botinfo.js
+++ commands/misc/botinfo.js
@@ -1,20 +1,13 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Colors } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, Colors } = require('discord.js');
 const { sql } = require("../../helpers/utils");
-
-const ticket_log = new ActionRowBuilder()
-    .addComponents(
-        new ButtonBuilder()
-            .setCustomId('ticket_log')
-            .setLabel("チケットLogチャンネルを削除")
-            .setStyle(ButtonStyle.Success)
-    );
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('botinfo')
         .setDMPermission(false)
         .setDescription('BOTの情報を表示します'),
     async execute(interaction) {
-        const getdata = await sql(`select * from ticket_channel where guildid="${interaction.guild.id}";`);
+        const ticketdata = await sql(`select * from ticket_channel where guildid="${interaction.guild.id}";`);
+        const logdata = await sql(`select * from log_channel where guildid="${message.guild.id}";`);
         const latency = new EmbedBuilder()
             .setTitle(`✅BOTの情報`)
             .setDescription("レイテンシー計測しています...")
@@ -25,14 +18,12 @@ module.exports = {
             .setTitle(`✅BOTの情報`)
             .addFields(
                 { name: 'Ping', value: `📡BOT応答時間:${msg.createdTimestamp - interaction.createdTimestamp}ws\n📡API応答時間:${interaction.client.ws.ping}ws` },
-                { name: 'Ticketlogチャンネル', value: getdata[0]?.guildid ? "設定済み" : "未設定" },
+                { name: 'Ticketlogチャンネル', value: ticketdata[0]?.guildid ? "設定済み" : "未設定" },
+                { name: 'logチャンネル', value: logdata[0]?.guildid ? "設定済み" : "未設定" },
                 { name: 'サポートサーバー', value: `https://discord.gg/Y6w5Jv3EAR` }
             )
             .setFooter({ iconURL: "https://media.discordapp.net/attachments/1081437402389811301/1082168221320364062/kuroneko.png", text: "©️ 2023 KURONEKOSERVER | botinfo" })
             .setColor(Colors.Green);
-        const send = { embeds: [embed], ephemeral: true };
-        const buttons = getdata[0]?.guildid ? ticket_log : null
-        if (buttons) send.components = [buttons];
-        await interaction.editReply(send);
+        await interaction.editReply({ embeds: [embed], ephemeral: true });
     }
 };
