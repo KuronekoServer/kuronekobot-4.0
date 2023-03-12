85f4a67be12a803048ca30d442842d33ae832def
TanakaTakeshikun <maru1679@icloud.com>
2023-03-12 11:09:40 +0900

    feature: BOTのステータス追加

--- events/misc/ready.js
+++ events/misc/ready.js
@@ -4,6 +4,9 @@ const chalk = require('chalk');
 module.exports = {
     name: Events.ClientReady,
     async execute(client) {
+        client.user.setActivity(`/help `, {
+            type: 'PLAYING',
+        });
         console.log(chalk.green('[成功]'), `${client.user.tag}にログインしました。`)
     }
 }
