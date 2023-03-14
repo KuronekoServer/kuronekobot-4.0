23897c77c1eb99f3bb22851e1cc68c0772ccba9a
anmoti <77442643+anmoti@users.noreply.github.com>
2023-03-14 16:23:13 +0900

    書き方の統一

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js');
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+const { Client, GatewayIntentBits, Collection, Partials } = require("discord.js");
+
+require("dotenv").config();
+
 const client = new Client({
     intents: Object.values(GatewayIntentBits),
     partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
-const chalk = require('chalk');
-require('dotenv').config();
-const fs = require('node:fs');
-const path = require('node:path');
-require('./deploy-commands.js');
 
-// イベントハンドラー
+require("./deploy-commands.js");
+
+// EventHandler
 client.events = new Collection();
-fs.readdirSync('./events/').forEach(async dir => {
-    const eventsPath = path.join(__dirname, `./events/${dir}`);
-    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-    for (const file of eventFiles) {
+fs.readdirSync(path.resolve(__dirname, "events")).forEach((dir) => {
+    const eventsPath = path.resolve(__dirname, "events", dir);
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+    eventFiles.forEach((file) => {
         const filePath = path.join(eventsPath, file);
         const event = require(filePath);
         client.on(event.name, (...args) => event.execute(...args));
-    };
+    });
 });
 
-// スラッシュコマンドハンドラー
+// SlashCommandHandler
 client.commands = new Collection();
-fs.readdirSync('./commands/').forEach(async dir => {
-    const commandsPath = path.join(__dirname, `./commands/${dir}`);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
+fs.readdirSync(path.resolve(__dirname, "commands")).forEach((dir) => {
+    const commandsPath = path.resolve(__dirname, "commands", dir);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+    commandFiles.forEach((file) => {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
 
-        if ('data' in command && 'execute' in command) {
+        if ("data" in command && "execute" in command) {
             client.commands.set(command.data.name, command);
         } else {
             console.log(chalk.red("[警告]"), `${filePath}はdataかexecuteプロパティが設定されていません。`);
-        };
-    };
+        }
+    });
 });
 
-//エラー後も処理継続
+// ErrorHandler
 process.on("uncaughtException", (reason, promise) => {
     console.log(chalk.red(`[エラー] ${reason}`));
 });
