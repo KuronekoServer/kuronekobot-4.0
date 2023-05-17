a01fdb90fd1710b27d1c3e2463f611bbcc4b0e0e
anmoti <77442643+anmoti@users.noreply.github.com>
2023-05-17 17:55:23 +0900

    slashcommandの実行を修正
    エラーの表示
    エラーのロガー
    AutoCompleteが機能していなかった

--- events/misc/slashcommand.js
+++ events/misc/slashcommand.js
@@ -5,38 +5,32 @@ module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
         const { client } = interaction;
+        const command = client.commands.get(interaction.commandName);
+        if (!command) return;
         if (interaction.isAutocomplete()) {
-            const command = client.commands.get(interaction.commandName);
             if (!command) return;
             try {
                 await command.autocomplete(interaction, command.logger);
             } catch (error) {
-                command.logger.error(`${error.message}\n${error.stack}`);
+                command.logger.error(error);
             }
-        } else {
-            const command = client.commands.get(interaction.commandName);
-            if (!command) return;
-            try {
-                const response = await command.execute(interaction, command.logger);
-                if (command.subcommands) {
-                    const subcommand = command.subcommands.get(interaction.options.getSubcommand());
-                    const args = response ?? [];
-                    subcommand.execute(interaction, ...args, command.logger);
-                }
-            } catch (error) {
-                command.logger.error(`${error.message}\n${error.stack}`);
-                const embed = new EmbedBuilder()
-                    .setTitle("⚠エラー")
-                    .setFooter({ iconURL: "https://media.discordapp.net/attachments/1081437402389811301/1082168221320364062/kuroneko.png", text: "© 2023 KURONEKOSERVER | SlashCommand" })
-                    .setColor(Colors.Red);
-                if (error.message === "Missing Permissions") {
-                    embed.setDescription("権限が足りません。\nBOTに権限を与えてください")
-                    interaction.reply({ embeds: [embed], ephemeral: true }).catch(() => { });
-                } else {
-                    embed.setDescription(`不明なエラーが発生しました。\n詳細:${error.message}\n運営に問い合わせていただけると幸いです。`)
-                    interaction.reply({ embeds: [embed], ephemeral: true }).catch(() => { });
-                }
+        }
+        try {
+            const response = await command.execute(interaction, command.logger);
+            if (command.subcommands) {
+            const subcommand = command.subcommands.get(interaction.options.getSubcommand());
+                const args = response ?? [];
+                subcommand.execute(interaction, ...args, command.logger);
             }
+        } catch (error) {
+            command.logger.error(error);
+            const embed = new CustomEmbed("error").typeError();
+            if (error.message === "Missing Permissions") {
+                embed.setDescription("権限が足りません。\nBOTに権限を与えてください")
+            } else {
+                embed.setDescription(`不明なエラーが発生しました。\n詳細:${error.message}\n運営に問い合わせていただけると幸いです。`)
+            }
+            interaction.reply({ embeds: [embed], ephemeral: true }).catch(() => { });
         }
     }
 };
