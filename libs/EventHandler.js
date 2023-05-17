1d75d0a1f6d319f172df613c96b1be1257de6984
anmoti <77442643+anmoti@users.noreply.github.com>
2023-05-17 17:33:33 +0900

    update event loading logger

--- libs/EventHandler.js
+++ libs/EventHandler.js
@@ -4,19 +4,23 @@ const logger = require("./GetLogger");
 
 function EventHandler(client, eventsPath) {
     const Log = logger.createChannel("event");
-    Log.debug("Loading...");
+    Log.info("Loading...");
     const events = [];
     fs.readdirSync(eventsPath).forEach((dir) => {
+        Log.debug(`Loading ${dir}...`);
         const eventPath = path.resolve(eventsPath, dir);
         const eventFiles = fs.readdirSync(eventPath).filter(file => file.endsWith(".js"));
         for (const file of eventFiles) {
+            Log.debug(`Loading ${dir} ${file}...`);
             const event = require(path.resolve(eventPath, file));
             event.logger = Log.createChild(event.name);
             client.on(event.name, (...args) => event.execute(...args, event.logger));
             events.push(event);
+            Log.debug(`Loaded ${dir} ${event.name} (${file})`);
         }
+        Log.debug(`Loaded ${eventFiles.length} events for ${dir}`);
     });
-    Log.debug(`Loaded ${events.length} events`);
+    Log.info(`Loaded ${events.length} events`);
     return events;
 }
 
