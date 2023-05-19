6e5bbc3d334b7894b50c03b8a68e8ab7434703d4
anmoti <77442643+anmoti@users.noreply.github.com>
2023-05-20 01:52:35 +0900

    msgURLの修正

--- events/misc/msgURL.js
+++ events/misc/msgURL.js
@@ -1,7 +1,7 @@
 const { Events, Colors, hyperlink } = require("discord.js");
-const { CustomEmbed } = require("../../libs");
+const { CustomEmbed, getEmbedName } = require("../../libs");
 
-const regex = /(https?:\/\/)?((ptb|canary)\.)?discord\.com\/channels(\/\d{17,19}){3}/;
+const regex = /(https?:\/\/)?((ptb|canary)\.)?discord\.com\/channels(\/\d{17,19}){3}/g;
 
 module.exports = {
     name: Events.MessageCreate,
@@ -9,32 +9,41 @@ module.exports = {
         const { client, content } = message;
         const urls = content.match(regex);
         if (!urls) return;
-        urls.forEach(async (url) => {
-            const [guildId, channelId, messageId] = new URL(url).pathname.split("/").slice(2);
-            const guild = client.guilds.cache.get(guildId);
+        const ids = urls
+            .map((url) => {
+                const paths = url.split("/");
+                const index = paths.indexOf("channels");
+                const [guild, channel, message] = paths.slice(index + 1);
+                return { guild, channel, message };
+            })
+            .filter((id, index, ids) => ids.findIndex((i) => i.guild === id.guild && i.channel === id.channel && i.message === id.message) === index);
+        
+        for (const id of ids) {
+            const guild = client.guilds.cache.get(id.guild);
             if (!guild) return;
-            const channel = await guild.channels.fetch(channelId);
+            const channel = guild.channels.cache.get(id.channel);
             if (!channel) return;
-            const msg = await channel.messages.fetch(messageId);
+            const msg = await channel.messages.fetch(id.message);
             if (!msg) return;
-            const embedText = msg.embeds.length ? `\n${msg.embeds.length}個の埋め込みメッセージがあります。` : "";
-            const attachmentText = msg.attachments.size ? `\n${msg.attachments.size}個の添付ファイルがあります。` : "";
+            const embeds = msg.embeds.filter((embed) => getEmbedName(embed) !== "fetchMessage");
+            const attachments = msg.attachments.map((attachment) => attachment);
+            let description = hyperlink("メッセージにジャンプ", msg.url);
+            if (embeds.length) description += `\n${embeds.length}個の埋め込みメッセージがあります。`;
+            if (attachments.length) description += `\n${attachments.length}個の添付ファイルがあります。`;
             const embed = new CustomEmbed("fetchMessage")
-                .setAuthor({ name: msg.author.tag, icon_url: msg.author.displayAvatarURL({ dynamic: true }) })
-                .setDescription(`${hyperlink("メッセージにジャンプ", msg.url)}${embedText}${attachmentText}`)
+                .setAuthor({ name: msg.author.tag, iconURL: msg.author.displayAvatarURL() })
+                .setDescription(description)
                 .setTimestamp(msg.createdAt)
                 .setColor(Colors.Aqua);
             if (msg.content) embed.addFields({ name: "\u200b", value: msg.content || "\u200b" });
-            if (msg.embeds.length) 
-        });
-        
+            message.reply({ embeds: [embed] });
+            if (embeds.length) {
+                embeds.forEach((embed, index) => {
+                    message.channel.send({ embeds: [embed], files: (embeds.length - 1 === index ? attachments : null) });
+                });
+            } else if (attachments.length) {
+                message.channel.send({ files: attachments })
+            }
+        }
     }
 };
-
-function fetchMessageData(url, client) {
-    const [guildId, channelId, messageId] = 
-    const guild = client.guilds.cache.get(guildId);
-    const channel = guild.channels.cache.get(channelId);
-    const msg = channel.messages.fetch(messageId);
-    return msg;
-}
