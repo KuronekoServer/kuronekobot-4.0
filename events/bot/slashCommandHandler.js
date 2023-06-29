64de39ce1424e13904e810a1077b5969563918bc
anmoti <77442643+anmoti@users.noreply.github.com>
2023-06-29 22:49:16 +0900

    fix: autocompleteが空でも返されるようにした

--- events/bot/slashCommandHandler.js
+++ events/bot/slashCommandHandler.js
@@ -8,11 +8,9 @@ module.exports = {
     async execute(interaction) {
         const command = new Command.SlashCommandManager(interaction);
 
-        if (interaction.isAutocomplete() && command.autocomplete && command.options.getFocused()) {
+        if (interaction.isAutocomplete() && command.autocomplete) {
             const result = await command.autocomplete(command);
-            if ((result ?? []).length > 0){
-                interaction.respond(result);
-            }
+            if (Array.isArray(result)) interaction.respond(result);
         }
 
         if (interaction.isCommand() && command.execute) {
