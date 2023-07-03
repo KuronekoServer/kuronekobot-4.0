65967a40137677f80162016e5bad573780df6f08
anmoti <77442643+anmoti@users.noreply.github.com>
2023-07-03 16:11:41 +0900

    fix: サーバーの時刻がずれていると正確にpingが取得できない問題

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -4,10 +4,21 @@ module.exports = {
         .setDescription("ボットの応答速度を測定します。")
     ,
     async execute(command) {
-        command.reply(
-            `:ping_pong:Pong!\n` +
-            `Websocket: ${command.client.ws.ping}ms\n` +
-            `API Endpoint: ${Date.now() - command.createdAt}ms`
-        );
+        let text = [
+            ':ping_pong:Pong!',
+            `Websocket: ${command.client.ws.ping}ms`,
+            'API Endpoint: 計測中...'
+        ];
+
+        const message = await command.reply({
+            content: text.join('\n'),
+            fetchReply: true
+        });
+        
+        text[2] = `API Endpoint: ${message.createdTimestamp - command.createdTimestamp - command.client.ws.ping}ms`;
+
+        message.edit({
+            content: text.join('\n')
+        });
     }
 };
